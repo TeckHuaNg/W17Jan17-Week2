60acbe9269a8d43374b95ae443e192818be75bfe
MichaelNg97 <michaelngteckhua@gmail.com>
2017-01-17 22:41:16 -0500

    Initial Commit

---
+++ src/w17jan/W17Jan.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package w17jan;
+
+/**
+ *
+ * @author Michael
+ */
+public class W17Jan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.printf("Hello World, my GitHub account name is TeckHuaNg");
+    }
+    
+}
